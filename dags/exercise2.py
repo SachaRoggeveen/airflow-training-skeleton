c3bb6d874b6b97a6b4328ea402262dea1646c328
sroggeveen <sroggeveen@bol.com>
2019-05-28 13:05:24 +0200

    added DAG daily

--- dags/exercise2.py
+++ dags/exercise2.py
@@ -1,6 +1,6 @@
 from airflow.operators.bash_operator import Bashoperator
-from airflow.operators.bash_operator import DummyOperator
-from airflow.operators.bash_operator import PythonOperator
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator PythonOperator
 
 args = {"owner": "sacha_roggeveen", "start_date": airflow.utils.dates.days_ago(14)}
 
