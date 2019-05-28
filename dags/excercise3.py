a16a34366d4b43bba2a7d551f8670a6a2db57263
sroggeveen <sroggeveen@bol.com>
2019-05-28 15:09:30 +0200

    added DAG daily

--- dags/excercise3.py
+++ dags/excercise3.py
@@ -2,6 +2,7 @@ import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
 from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.python_operator import PythonOperator
 from airflow.operators.python_operator import BranchPythonOperator
 from airflow.utils.trigger_rule import TriggerRule
 
