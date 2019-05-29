9a4f5e0c6708132c871ddde32c57b6ff484dfccd
sroggeveen <sroggeveen@bol.com>
2019-05-29 11:08:09 +0200

    added DAG daily

--- dags/exercise5.py
+++ dags/exercise5.py
@@ -14,22 +14,22 @@ from airflow.utils.decorators import apply_defaults
 
 
 
-
-class MyOwnHook(BaseHook):
-    def __init__(self, conn_id):
-        super().__init__(source=None)
-        self._conn_id = conn_id
-        self._conn = None
-
-    def get_conn(self):
-        if self._conn is None:
-            self._conn = object()  # create connection instance here
-            return self._conn
-
-    def do_stuff(self, arg1, arg2, **kwargs):
-        session = self.get_conn()
-        session.do_stuff()
-
+#
+# class MyOwnHook(BaseHook):
+#     def __init__(self, conn_id):
+#         super().__init__(source=None)
+#         self._conn_id = conn_id
+#         self._conn = None
+#
+#     def get_conn(self):
+#         if self._conn is None:
+#             self._conn = object()  # create connection instance here
+#             return self._conn
+#
+#     def do_stuff(self, arg1, arg2, **kwargs):
+#         session = self.get_conn()
+#         session.do_stuff()
+#
 
 
 
